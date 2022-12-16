03ac35dded80ea41fd929e8596cf118485bbb04b
plabarta <pablitolabarta@gmail.com>
2022-12-16 17:56:08 -0300

    intento de cambiar address

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,15 @@ pub mod query;
 pub mod sections;
 pub mod shroom;
 
-use std::collections::HashMap;
-
-use chart::*;
-use gloo_net::http::Request;
-use shroom::*;
 use yew::prelude::*;
 
 use crate::sections::{InOut, Netflow, TopInteractions, TotalTxs, TxsPorMes};
 
-// #[tokio::main]
-// async fn main() {
-//     println!("Sending req");
-
-//     let query_string = "select sum(amount_usd) from ethereum.core.ez_token_transfers
-//   where to_address = lower('0x65392485b8d869e59b5b2a3cf7de815ed16939aa')
-//   and block_timestamp::date >= '2022-01-01'"
-//         .to_string();
-
-//     let query_id = post_query(&query_string).await;
-//     let token = extract_token(&query_id);
-//     println!("Query ID: {}", token);
-
-//     let query_result = wait_for_result(&token).await;
-//     println!("Query Result: {}", query_result);
-// }
-
 #[function_component(App)]
 pub fn app() -> Html {
-    let address = "0x65392485b8d869e59b5b2a3cf7de815ed16939aa".to_string();
+    // Este Address no se puede modificar
+    let address: UseStateHandle<String> =
+        use_state(|| "0x86d3e894b5cdb6a80afffd35ed348868fb98dd3f".to_owned());
 
     html! {
         <>
@@ -52,25 +32,21 @@ pub fn app() -> Html {
                 </div>
             </section>
 
-            <TotalTxs address = {address.clone()} />
+            <section>
+                <p class="dialogo">{"Tu address de Ethereum es:"}</p>
+                <p class="dialogo">{{(*address).clone()}}</p>
+            </section>
 
-            <Netflow address = {address.clone()} />
+            <TotalTxs address = {(*address).clone()} />
 
-            <TxsPorMes address = {address.clone()} />
+            <Netflow address = {(*address).clone()} />
 
-            <TopInteractions address = {address.clone()} />
+            <TxsPorMes address = {(*address).clone()} />
 
-            <InOut address = {address.clone()} />
+            <TopInteractions address = {(*address).clone()} />
 
-            // <button>{"Draw chart"}</button>
-            // <svg id="chart" style="width: 100%; height: 800px;"></svg>
-            // <Chart />
+            <InOut address = {(*address).clone()} />
 
-            // Cantidad de TXs
-
-            // Cantidad de TXs por mes
-
-            //
         </>
     }
 }
